ae13263a9cc77d45296433d135ab904143d36a81
eddydiep <edouard.diep@gmail.com>
2019-09-16 02:56:53 +0200

    V2.0 - Major update + Diplomes et formations added

--- js/agency.js
+++ js/agency.js
@@ -1,22 +1,22 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
           scrollTop: (target.offset().top - 54)
-        }, 1000, "easeInOutExpo");
+        }, 800, "easeInOutCubic");
         return false;
       }
     }
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -27,7 +27,7 @@
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
@@ -40,3 +40,36 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
+
+// typing animation
+(function ($) {
+  $.fn.delay(1000).writeText = function (content) {
+    var contentArray = content.split(""),
+      current = 0,
+      elem = this;
+    setInterval(function () {
+      if (current < contentArray.length) {
+        elem.text(elem.text() + contentArray[current++]);
+      }
+    }, 80);
+  };
+
+})(jQuery);
+
+// input text for typing animation 
+$("#title-writer").writeText("Welcome to my portfolio");
+
+// button about animation
+$('#btn-about').delay(2500).animate({
+  'opacity': '1',
+  'margin-top': '15px'
+}, 1500);
+
+var percent = 80;
+$("#progressbar")
+  .animate({
+    "value": percent + "%"
+  }, {
+    duration: 600,
+    easing: 'linear'
+  });
