8ec35a8aefbbf7085386d5390dfcdcd2531d942d
eddydiep <edouard.diep@gmail.com>
2019-10-22 01:32:28 +0200

    V13.0 - Update Contact form + CSS

--- js/script.js
+++ js/script.js
@@ -22,6 +22,25 @@ var aboutQualifier = $('#about #profilQualifier');
 var lineSkillsVisible = false;
 var lineLangVisible = false;
 
+// disabling pinch zoom
+
+(function($) {
+  $.fn.nodoubletapzoom = function() {
+      $(this).bind('touchstart', function preventZoom(e) {
+        var t2 = e.timeStamp
+          , t1 = $(this).data('lastTouch') || t2
+          , dt = t2 - t1
+          , fingers = e.originalEvent.touches.length;
+        $(this).data('lastTouch', t2);
+        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+        e.preventDefault(); // double tap - prevent the zoom
+        // also synthesize click events we just swallowed up
+        $(this).trigger('click').trigger('click');
+      });
+  };
+})(jQuery);
+
 (function ($) {
   "use strict"; // Start of use strict
 
